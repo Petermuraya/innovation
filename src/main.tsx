5b96ed3279c47ac996e26d5e024092c143d24e6c
petermuraya <134588157+Petermuraya@users.noreply.github.com>
2025-06-03 14:20:44 +0000

    added error message on main incase if we do not load app.tsx Root container not found. Make sure your index.html has a div with id="root"'

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById('root')
+
+if (!container) {
+  throw new Error('Root container not found. Make sure your index.html has a div with id="root"')
+}
+
+const root = createRoot(container)
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
