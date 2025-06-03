72beb4ba11f6c52cd95388c464d55d3f267162aa
petermuraya <134588157+Petermuraya@users.noreply.github.com>
2025-06-03 12:02:18 +0000

    configured vite for collapsing pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/innovation/',      // <---- Add this line for GitHub Pages base path
+  base: '/innovation/',      
   server: {
     host: "::",
     port: 8080,
