5ef0eee93da874fd641941ab47863c55ab729718
petermuraya <134588157+Petermuraya@users.noreply.github.com>
2025-07-01 07:26:25 +0300

    Update vite.config.ts just removed inovation from the base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/innovation/',      
+  base: '/',      
   server: {
     host: "::",
     port: 8080,
